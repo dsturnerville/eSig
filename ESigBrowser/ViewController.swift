14008a277161541e6fbee07d7e49fc8034f7480f
dsturnerville <dsturnerville@gmail.com>
2016-09-15 14:32:53 -0500

    Update
    Update

--- ESigBrowser/ViewController.swift
+++ ESigBrowser/ViewController.swift
@@ -14,39 +14,36 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKSc
     var webView: WKWebView!
     var ncpView: WKWebView!
     
-    let webFrame = CGRectMake(0, 20, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height - 20)
+    let webFrame = CGRect(x: 0, y: 20, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - 20)
     
-    //let defaultURL = NSUserDefaults.standardUserDefaults().stringForKey("default_url")
+    var defaultURL = UserDefaults().string(forKey: "default_url")
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        registerSettingsBundle()
-        
-        let url = NSURL(string: "http://mymrpayroll.com")
-        
+        registerSettingsBundle()
+        let url = URL(string: (defaultURL)!)
         let configuration = WKWebViewConfiguration()
         configuration.preferences.javaScriptEnabled = true
         configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
-        configuration.userContentController.addScriptMessageHandler(self, name: "redirect")
+        configuration.userContentController.add(self, name: "done")
         webView = WKWebView(frame: view.bounds, configuration: configuration)
         self.webView.navigationDelegate = self
-        self.webView.UIDelegate = self
+        self.webView.uiDelegate = self
         view.addSubview(webView)
-        webView.loadRequest(NSURLRequest(URL:url!))
+        webView.load(URLRequest(url:url!))
     }
     
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
         webView.frame = webFrame
     }
     
-    func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         // handle the message
         if let body = message.body as? String {
-            print(body)
+            print("Script message: \"\(body)\"")
             switch body {
             case "close":
                 ncpView?.removeFromSuperview()
@@ -61,36 +58,31 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKSc
     // registers Settings Bundle
     func registerSettingsBundle(){
         let appDefaults = [String:AnyObject]()
-        NSUserDefaults.standardUserDefaults().registerDefaults(appDefaults)
+        UserDefaults.standard.register(defaults: appDefaults)
     }
 
     // this handles target=_blank & window.open links by opening them in a new view
-    func webView(webView: WKWebView,
-        createWebViewWithConfiguration configuration: WKWebViewConfiguration,
-        forNavigationAction navigationAction: WKNavigationAction,
+    func webView(_ webView: WKWebView,
+        createWebViewWith configuration: WKWebViewConfiguration,
+        for navigationAction: WKNavigationAction,
         windowFeatures: WKWindowFeatures) -> WKWebView? {
             if navigationAction.targetFrame == nil {
-                let url = navigationAction.request.URL
+                let url = navigationAction.request.url
                 ncpView = WKWebView(frame: webFrame)
-                ncpView.loadRequest(NSURLRequest(URL:url!))
+                ncpView.load(URLRequest(url:url!))
                 self.view.addSubview(ncpView)
-            } else {
-                let url = NSURL(string: "http://www.cashamerica.com")
-                ncpView = WKWebView(frame: webFrame)
-                ncpView.loadRequest(NSURLRequest(URL:url!))
-                self.view.addSubview(ncpView)
-            }
+            } 
             return nil
     }
     
     
-    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
-        let script = "function closeWindowFromApp() {window.webkit.messageHandlers.redirect.postMessage('close');}"
-        webView.evaluateJavaScript(script, completionHandler: ({
-            (result, error) in
-            print("result: \(result), error: \(error)")
-        }))
-    }
+//    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//        let script = "function closeWindowFromApp() {window.webkit.messageHandlers.redirect.postMessage('close');}"
+//        webView.evaluateJavaScript(script, completionHandler: ({
+//            (result, error) in
+//            print("result: \(result), error: \(error)")
+//        }))
+//    }
     
     
 }
